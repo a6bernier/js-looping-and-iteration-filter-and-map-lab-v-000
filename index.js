e52f02a274410395ffdf29c3f3b4c0d116d55368
Andrew Bernier <a6bernier@gmail.com>
2018-12-17 19:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function driversWithRevenueOver(drivers, revenue){
-  return driver.filter(function(driver){
+  return drivers.filter(function(driver){
     return driver.revenue > revenue
   })
 }
